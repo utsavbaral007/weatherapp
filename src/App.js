a240e98693818b16027395199c2b3f5f980f74a0
Utsav Baral <manutsssav@gmail.com>
2020-04-03 14:47:28 +0545

    weather app no bugs fixed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Weather from './components/weather'
+import Form from './components/form'
+import Axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const apiKey = 'b15e366e446a5d0843df7ca7c36c31e5'
+
+export default function App() {
+	const [city, setCity] = useState('')
+	const [country, setCountry] = useState('')
+	const [temperature, setTemperature] = useState('')
+	const [minTemp, setMinTemp] = useState('')
+	const [maxTemp, setMaxTemp] = useState('')
+	const [status, setStatus] = useState('')
+	const [iconId, setIconId] = useState('')
+	const [error, setError] = useState(false)
+
+	const calCelcius = temp => {
+		let cel = Math.floor(temp - 273.15)
+		return cel
+	}
+	const getWeather = async e => {
+		e.preventDefault()
+		const city = e.target.elements.city.value
+		const country = e.target.elements.country.value
+		if (city && country) {
+			const weatherData = await Axios.get(
+				`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`
+			)
+			const weatherReport = weatherData.data
+			console.log(weatherReport)
+			setCity(weatherReport.name)
+
+			setTemperature(calCelcius(weatherReport.main.temp))
+			setMinTemp(calCelcius(weatherReport.main.temp_min))
+			setMaxTemp(calCelcius(weatherReport.main.temp_max))
+			setStatus(weatherReport.weather[0].description)
+			setIconId(weatherReport.weather[0].id)
+			setCountry(weatherReport.sys.country)
+		} else setError(true)
+	}
+	console.log(city)
+	console.log(country)
+	return (
+		<div className="App">
+			<div style={{ padding: '50px' }}>
+				<Form
+					loadWeather={getWeather}
+					error={error}
+					city={city}
+					country={country}
+				/>
+			</div>
+			<Weather
+				city={city}
+				country={country}
+				temperature={temperature}
+				minTemp={minTemp}
+				maxTemp={maxTemp}
+				status={status}
+				iconId={iconId}
+			/>
+		</div>
+	)
 }
-
-export default App;
---
+++ src/components/weather.js
@@ -0,0 +1,22 @@
+import React from 'react'
+export default function Weather(props) {
+	return (
+		<div className="container">
+			<h1>{props.city}</h1>
+			{props.temperature ? <h1>{props.temperature}&deg;</h1> : null}
+			{minmaxTemp(props.minTemp, props.maxTemp)}
+			<h3>{props.status}</h3>
+		</div>
+	)
+}
+function minmaxTemp(min, max) {
+	if (min && max) {
+		return (
+			<h3>
+				<span className="minTemp">min:&nbsp;{min}&deg;</span>{' '}
+				&nbsp;&nbsp;&nbsp;&nbsp;
+				<span className="maxTemp">max:&nbsp;{max}&deg;</span>
+			</h3>
+		)
+	}
+}
